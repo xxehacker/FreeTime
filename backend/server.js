289bd5a3d5f86d9e25739b83a9f003f8a88be1a1
Mridupawan Bordoloi <mridupawan503@gmail.com>
2024-12-24 20:01:55 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import { ENV_VARIABLES } from "./config/envVariables.js";
 import { connectDB } from "./config/mongoconnect.js";
 import cookieParser from "cookie-parser";
 // import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = ENV_VARIABLES.PORT;
+const __dirname = path.resolve();
 
 // All Routes
 import authRoutes from "./routes/auth.route.js";
@@ -14,14 +16,25 @@ import tvshowRoutes from "./routes/tvshow.route.js";
 import searchRoutes from "./routes/search.route.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
+
 
 // app.use(cors({
 //   origin: 'http://localhost:5173', // Allow requests from the frontend server
 //   credentials: true,
 // }));
+
+
 // All Middlewares
 app.use(express.json()); // for parsing JSON. its means that we can use req.body to get the data as JSON
 // app.use(express.urlencoded({ extended: true })); // for parsing form data . its means that we can use req.body to get the form data.
+
+
 app.use(cookieParser()); // for parsing cookies . its means that we can use req.cookies to get the cookies.
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
